a4118a2bb8fe5e4bf662f2dfb04824a8f462f2e7
bva-bme <62710558+bva-bme@users.noreply.github.com>
2023-01-31 11:25:39 +0100

    Add files via upload

---
+++ examples/town/simulate_town_cosim.py
@@ -0,0 +1,128 @@
+import sys, os
+import random
+import time
+import pickle
+from libsumo_parallel import *
+
+sys.setrecursionlimit(25000)
+
+
+def micro_callback(ego_id, init, edges):
+
+    if init:
+        # Initialize ego vehicle
+        route_edges = ['-74725439#1', '-74725439#0', '-74725447', '-467182459', '325782603#0', '325782603#1',
+                       '325782603#2', '325782603#4', '765276434', '-300832373#5', '-765276438#1', '-765276438#0']
+        libsumo.route.add('ego_route', route_edges)
+        libsumo.vehicle.add(ego_id, 'ego_route')
+
+    # Reroute ego vehicle on a random route
+    try:
+        current_route = libsumo.vehicle.getRoute(ego_id)
+        current_edge = libsumo.vehicle.getRoadID(ego_id)
+    except libsumo.TraCIException as e:
+        libsumo.vehicle.add(ego_id, 'ego_route')
+        st_exception = True
+        while st_exception:
+            try:
+                target_edge = random.choice(edges)
+                libsumo.vehicle.changeTarget(ego_id, target_edge)
+                current_route = libsumo.vehicle.getRoute(ego_id)
+                if len(current_route) > 2:
+                    st_exception = False
+                    current_route = libsumo.vehicle.getRoute(ego_id)
+                    current_edge = libsumo.vehicle.getRoadID(ego_id)
+            except libsumo.TraCIException:
+                pass
+    if current_edge == current_route[-2]:
+        st_exception = True
+        while st_exception:
+            try:
+                target_edge = random.choice(edges)
+                libsumo.vehicle.changeTarget(ego_id, target_edge)
+                current_route = libsumo.vehicle.getRoute(ego_id)
+                if len(current_route) > 2:
+                    st_exception = False
+            except libsumo.TraCIException:
+                break
+
+    # Get every vehicle speed (to interact with the simulation)
+    vehs = libsumo.vehicle.getIDList()
+    speeds = []
+    for veh in vehs:
+        speeds.append(libsumo.vehicle.getSpeed(veh))
+
+    # logging
+    headway_thd = 150  # m
+    veh_count = len(vehs)
+    try:
+        ego_speed = libsumo.vehicle.getSpeed(ego_id)
+        if libsumo.vehicle.couldChangeLane(ego_id, 1) or libsumo.vehicle.couldChangeLane(ego_id, -1):
+            changed_lane = 1
+        else:
+            changed_lane = 0
+        headway = libsumo.vehicle.getLeader(ego_id, headway_thd)
+    except libsumo.TraCIException:
+        ego_speed = 0
+        changed_lane = 0
+        headway = None
+
+    return veh_count, ego_speed, changed_lane, headway
+
+
+def main():
+
+    micro = True
+    init = True
+    ego_id = 'ego'
+
+    # Get the road network as graph
+    script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+    sumo_cmd = ["sumo", "-c", script_dir + "/town_scenario/town.sumocfg", "--start"]
+    network_path = script_dir + "/town_scenario/town.net.xml"
+    parallel_conn = LibsumoParallelConnection(micro_callback, None)
+    network = parallel_conn.parse_network(network_path)
+    edge_objs = network.getEdges()
+    edges = []
+    for e in edge_objs:
+        edges.append(e.getID())
+    micro_cmd, meso_cmd = parallel_conn.create_meso(sumo_cmd)
+    parallel_conn.start(micro_cmd, meso_cmd, ego_id, 250)
+
+    log_veh_count = []
+    log_ego_speed = []
+    log_num_lane_changes = 0
+    log_headway = []
+
+    t1 = time.time()
+    for i in range(3600):
+
+        # Step both simulators
+        if i == 0:
+            parallel_conn.set_callback_arguments((ego_id, init, edges), micro)
+        else:
+            parallel_conn.set_callback_arguments((ego_id, not init, edges), micro)
+
+        parallel_conn.simulation_step(network)
+
+        veh_count, ego_speed, changed_lane, headway = parallel_conn.get_callback_returns(micro)
+
+        # logging
+        log_veh_count.append(veh_count)
+        log_ego_speed.append(ego_speed)
+        log_num_lane_changes += changed_lane
+        log_headway.append(headway)
+
+    t2 = time.time()
+    print(f"Simulation time: {t2 - t1} seconds")
+
+    log_time = t2-t1
+
+    with open(script_dir + "/results/town_cosim_results", 'wb') as f:
+        pickle.dump([log_time, log_veh_count, log_ego_speed, log_num_lane_changes, log_headway], f)
+
+    parallel_conn.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ examples/town/simulate_town_micro.py
@@ -0,0 +1,92 @@
+import sys
+import os
+import libsumo as traci
+import random
+import time
+import pickle
+
+
+def main():
+
+    # Establish connection
+    script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+    sumo_cmd = ["sumo", "-c", script_dir + "/town_scenario/town.sumocfg", "--start"]
+    traci.start(sumo_cmd)
+    edges = traci.edge.getIDList()
+
+    # Initialize ego vehicle
+    route_edges = []
+    while not route_edges:
+        start = random.choice(edges)
+        destination = random.choice(edges)
+        route_edges = list(traci.simulation.findRoute(start, destination).edges)
+    traci.route.add('ego_route', route_edges)
+    traci.vehicle.add('ego', 'ego_route')
+
+    log_veh_count = []
+    log_ego_speed = []
+    log_num_lane_changes = 0
+    log_headway = []
+
+    t1 = time.time()
+    for i in range(3600):
+
+        # Step simulator
+        traci.simulationStep()
+
+        # Get every vehicle speed (to interact with the simulation)
+        vehs = traci.vehicle.getIDList()
+        speeds = []
+        for veh in vehs:
+            speeds.append(traci.vehicle.getSpeed(veh))
+
+        # Reroute ego vehicle on a random route
+        try:
+            current_route = traci.vehicle.getRoute('ego')
+            current_edge = traci.vehicle.getRoadID('ego')
+        except traci.TraCIException as e:
+            traci.vehicle.add('ego', 'ego_route')
+            st_exception = True
+            while st_exception:
+                try:
+                    target_edge = random.choice(edges)
+                    traci.vehicle.changeTarget('ego', target_edge)
+                    current_route = traci.vehicle.getRoute('ego')
+                    if len(current_route) > 2:
+                        st_exception = False
+                        current_route = traci.vehicle.getRoute('ego')
+                        current_edge = traci.vehicle.getRoadID('ego')
+                except traci.TraCIException:
+                    pass
+        if current_edge == current_route[-2]:
+            st_exception = True
+            while st_exception:
+                try:
+                    target_edge = random.choice(edges)
+                    traci.vehicle.changeTarget('ego', target_edge)
+                    current_route = traci.vehicle.getRoute('ego')
+                    if len(current_route) > 2:
+                        st_exception = False
+                except traci.TraCIException:
+                    break
+
+        # logging
+        log_veh_count.append(len(vehs))
+        log_ego_speed.append(traci.vehicle.getSpeed('ego'))
+        if traci.vehicle.couldChangeLane('ego', 1) or traci.vehicle.couldChangeLane('ego', -1):
+            log_num_lane_changes += 1
+        log_headway.append(traci.vehicle.getLeader('ego', 150))
+
+    t2 = time.time()
+    print(f"Simulation time: {t2 - t1} seconds")
+
+    log_time = t2 - t1
+
+    with open(script_dir + '/results/town_micro_results', 'wb') as f:
+        pickle.dump([log_time, log_veh_count, log_ego_speed, log_num_lane_changes, log_headway], f)
+
+    traci.close()
+
+
+if __name__ == "__main__":
+    main()
